0e93b688c15f6db9b75d9429aea45bc34fbbeb8c
prakashpandit <rajprakash8294@gmial.com>
2025-07-20 11:06:53 +0530

    responsice in mobile phone

--- Analog clock/index.js
+++ Analog clock/index.js
@@ -20,3 +20,19 @@ function displayTime() {
 }
 
 setInterval(displayTime, 1000);
+
+/*
+  hours ke liye formula = 12 hour bajane ke liye 360deg rotate hoga
+   to 1 hour ke liye =(360deg/12) = 30deg
+
+   (1) formula  = for  h hour = (30h+m/2)
+
+  minute  ke liye formula  = 60 minute rotation karane ke liye 360deg rotate hoga
+  to 1 minute ke liye = (360/60) = 6 deg
+  (2)  formula = for m minute = (m/2)  
+  
+  second ke liye formula = 60 second rotation karane ke liye 360 deg rotate hoga
+  to 1 second  ke liye = (360/60) = 6 deg
+
+*/
+
